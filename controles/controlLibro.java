1d995d751f53f7192bf787935c905e7ca30aed1f
ricardolicea <ricardo.liceamata@gmail.com>
2017-01-27 10:56:17 -0600

    Controles

---
+++ controles/controlLibro.java
@@ -0,0 +1,16 @@
+package controles;
+import entidades.libro;
+
+public class controlLibro {
+  Libro libro;
+  public controlLibro(){
+    libro = new Libro();
+  }
+  public void agregaLibro(int idLibro, int topografico, String titulo, int iISBN,
+   String tituloOriginal, String subtitulo, String edicion, String lugarPubli,
+   int anoPubli, String notas, int numCopias, int costo, int numPaginas,
+   String proveedor, Date adquisicion, boolean ilustraciones, boolean graficas,
+   boolean mapas, boolean indices, boolean planas, boolean bibliografia,
+   autores autor)
+   libro.
+}
